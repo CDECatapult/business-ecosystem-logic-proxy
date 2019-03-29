c2e8c408121df545242230cbdb53deb5700d2747
Anthony Garcia <anthony.garcia@digicatapult.org.uk>
2019-03-29 14:40:13 +0000

    Revert "Remove fill_indexes"
    This reverts commit 25807c1f0417af4539e6c76c1a17e7fb81634238.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -67,7 +67,8 @@ done
 # Include this setting to avoid inconsistencies between docker container port and used port
 sed -i "s|config\.port|config\.extPort|" /business-ecosystem-logic-proxy/lib/tmfUtils.js
 
-echo "Build frontend..."
+echo "Creating indexes..."
+node fill_indexes.js
 node collect_static.js
 
 node server.js
